b06e5ad2eb7a8a303f5b792fb5a073e2321e32b4
Vinicius Pegorini <viniciuspegorini@gmail.com>
2025-04-01 21:06:20 -0300

    2025-04-01 - Desenvolvimento com TDD no back-end com Spring Framework.

--- server/src/main/java/br/edu/utfpr/pb/pw44s/server/model/User.java
+++ server/src/main/java/br/edu/utfpr/pb/pw44s/server/model/User.java
@@ -1,13 +1,21 @@
 package br.edu.utfpr.pb.pw44s.server.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
+@Table(name = "tb_user")
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
     private String displayName;
     private String username;
     private String password;
--- server/src/test/java/br/edu/utfpr/pb/pw44s/server/UserControllerTest.java
+++ server/src/test/java/br/edu/utfpr/pb/pw44s/server/UserControllerTest.java
@@ -1,18 +1,57 @@
 package br.edu.utfpr.pb.pw44s.server;
 
 import br.edu.utfpr.pb.pw44s.server.model.User;
+import br.edu.utfpr.pb.pw44s.server.repository.UserRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+// @TestMethodOrder(MethodOrderer.MethodName.class)
 public class UserControllerTest {
 
+    @Autowired
+    TestRestTemplate restTemplate;
+    @Autowired
+    UserRepository userRepository;
+    
+    @BeforeEach
+    public void cleanUp() {
+        userRepository.deleteAll();
+        restTemplate.getRestTemplate().getInterceptors().clear();
+    }
+
     //methodName_condition_expectedBehaviour
+    @Test
     public void postUser_whenUserIsValid_receiveOk() {
         User user = new User();
         user.setDisplayName("test-Display");
         user.setUsername("test-user");
         user.setPassword("P4ssword");
+
+        ResponseEntity<Object> response =
+                restTemplate.postForEntity("/users", user, Object.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void postUser_whenUserIsValid_userSavedToDatabase() {
+        User user = new User();
+        user.setDisplayName("test-Display");
+        user.setUsername("test-user");
+        user.setPassword("P4ssword");
+
+        ResponseEntity<Object> response =
+                restTemplate.postForEntity("/users", user, Object.class);
+
+        assertThat( userRepository.count() ).isEqualTo( 1 );
     }
 }
